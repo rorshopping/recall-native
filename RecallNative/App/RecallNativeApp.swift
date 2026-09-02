c5f5ca0b813152bc1b66d98b554e4dcaeb9e7435
Richard Bäcker <35996656+rorshopping@users.noreply.github.com>
2026-09-02 11:52:48 +0200

    feat: receive background model download events

--- RecallNative/App/RecallNativeApp.swift
+++ RecallNative/App/RecallNativeApp.swift
@@ -1,8 +1,19 @@
 import SwiftUI
 import SwiftData
+import UIKit
+
+final class RecallNativeAppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     handleEventsForBackgroundURLSession identifier: String,
+                     completionHandler: @escaping () -> Void) {
+        guard identifier == BackgroundModelDownloader.sessionIdentifier else { return }
+        BackgroundModelDownloader.shared.handleBackgroundEvents(completionHandler: completionHandler)
+    }
+}
 
 @main
 struct RecallNativeApp: App {
+    @UIApplicationDelegateAdaptor(RecallNativeAppDelegate.self) private var appDelegate
     @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
 
     var body: some Scene {
